818db78d3acd468172cf94294e0cffff4fffcba5
Chobaka78 <34846941+chobaka78@users.noreply.github.com>
2019-06-14 00:33:05 -0400

    Maps all in

--- core/src/com/mygdx/game/WorldCreator.java
+++ core/src/com/mygdx/game/WorldCreator.java
@@ -101,6 +101,48 @@ public class WorldCreator {
 
         }
 
+        for(MapObject obj : map.getLayers().get(8).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) obj).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+
+            bdef.position.set(rect.getX() * Main.PPM + rect.getWidth() / 2 * Main.PPM, rect.getY() * Main.PPM + rect.getHeight() / 2 * Main.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 * Main.PPM, rect.getHeight() / 2 * Main.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef).setUserData("Building-Intraction");
+
+        }
+
+        for(MapObject obj : map.getLayers().get(9).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) obj).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+
+            bdef.position.set(rect.getX() * Main.PPM + rect.getWidth() / 2 * Main.PPM, rect.getY() * Main.PPM + rect.getHeight() / 2 * Main.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 * Main.PPM, rect.getHeight() / 2 * Main.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef).setUserData("Mapchange");
+
+        }
+
+        for(MapObject obj : map.getLayers().get(10).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) obj).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+
+            bdef.position.set(rect.getX() * Main.PPM + rect.getWidth() / 2 * Main.PPM, rect.getY() * Main.PPM + rect.getHeight() / 2 * Main.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 * Main.PPM, rect.getHeight() / 2 * Main.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef).setUserData("Map2");
+
+        }
+
     }
 
 }
